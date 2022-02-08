8ba077ae2210b438c4c04990ecaba6aa86a8061a
web-novice-tech <itb4769we@gmail.com>
2022-02-08 10:39:55 +0900

    pip install django-bootstrap5

--- django_snippets/settings.py
+++ django_snippets/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'snippets.apps.SnippetsConfig'
+    'django_bootstrap5',
+    'snippets.apps.SnippetsConfig',
 ]
 
 MIDDLEWARE = [
